f8b146b8bbc0cbc5b3dd2e0d118b2fbc51abc544
Nicolas Tobar <nicolas.tobar@ug.uchile.cl>
2023-07-24 11:47:18 -0400

    docs

--- exploring.R
+++
@@ -1,7 +0,0 @@
-pacman::p_load(haven)
-
-school_chile <- haven::read_sas("./ICILS2013_IDB_SAS/Data/bcgchli1.sas7bdat")
-student_chile <- haven::read_sas("./ICILS2013_IDB_SAS/Data/bsgchli1.sas7bdat")
-teacher_chile <- haven::read_sas("./ICILS2013_IDB_SAS/Data/btgchli1.sas7bdat")
-
-
